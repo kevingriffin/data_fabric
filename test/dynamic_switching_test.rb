814789ebbbcbe1d01cb3bd5915e9df9cef79270f
Kevin Griffin <xgriffin.kevin@gmail.com>
2012-09-28 18:11:26 +0900

    Update tests to use new database names.

--- test/dynamic_switching_test.rb
+++ test/dynamic_switching_test.rb
@@ -59,17 +59,17 @@ class DynamicSwitchingTest < Test::Unit::TestCase
 
   def test_reads_from_slave_when_below_threshold
     ReplicateModel.connection.status_checker.poller = PollerMock.new(false)
-    assert_equal "test_slave", ReplicateModel.find(1).name
+    assert_equal "df_test_slave", ReplicateModel.find(1).name
   end
 
   def test_reads_from_master_when_above_threshold
     ReplicateModel.connection.status_checker.poller = PollerMock.new(true)
-    assert_equal "test_master", ReplicateModel.find(1).name
+    assert_equal "df_test_master", ReplicateModel.find(1).name
   end
 
   def test_with_master_always_goes_to_master
     ReplicateModel.connection.status_checker.poller = PollerMock.new(false)
-    assert_equal "test_master", ReplicateModel.with_master() { ReplicateModel.find(1).name }
+    assert_equal "df_test_master", ReplicateModel.with_master() { ReplicateModel.find(1).name }
   end
 
   def test_with_master_can_be_nested
@@ -79,7 +79,7 @@ class DynamicSwitchingTest < Test::Unit::TestCase
     result = ReplicateModel.with_master do
       ReplicateModel.find(1).name + ReplicateModel.with_master { ReplicateModel.find(1).name } + AnotherReplicateModel.with_master { AnotherReplicateModel.find(1).name}
     end
-    assert_equal "test_mastertest_mastertest_master", result
+    assert_equal "df_test_masterdf_test_masterdf_test_master", result
   end
 
   def test_with_slave_can_be_nested
@@ -88,12 +88,12 @@ class DynamicSwitchingTest < Test::Unit::TestCase
     result = ReplicateModel.first.name + ReplicateModel.with_slave do
       ReplicateModel.with_slave { ReplicateModel.first.name } + ReplicateModel.first.name
     end + ReplicateModel.first.name
-    assert_equal("test_mastertest_slavetest_slavetest_master", result, "Slave should not swap when in a fixed role block.")
+    assert_equal("df_test_masterdf_test_slavedf_test_slavedf_test_master", result, "Slave should not swap when in a fixed role block.")
   end
 
 
   def test_find_in_batches_doesnt_swap_during_a_find_when_inside_current_db
     ReplicateModel.connection.status_checker.poller = TimedPollerMock.new(1)
-    ReplicateModel.with_current_db { ReplicateModel.find_in_batches(:batch_size => 1) { |batch| sleep 0.5; assert_equal "test_slave", batch.first.name } }
+    ReplicateModel.with_current_db { ReplicateModel.find_in_batches(:batch_size => 1) { |batch| sleep 0.5; assert_equal "df_test_slave", batch.first.name } }
   end
 end
